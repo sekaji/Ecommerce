1c72efbd59b70c329a4604f5560f00440dcc86f5
sekajikmaynard@gmail.com <59362984+sekaji@users.noreply.github.com>
2021-10-13 18:05:30 -0400

    created up until beginning of newletter

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Announcements from '../components/Announcements'
+import Categories from '../components/Categories'
+import Navbar from '../components/Navbar'
+import Products from '../components/Products'
+import Slider from '../components/Slider'
+
+const Home = () => {
+    return (
+        <div>
+            <Announcements />
+            <Navbar/>
+            <Slider/>
+            <Categories/>
+            <Products />
+        </div>
+    )
+}
+
+export default Home
