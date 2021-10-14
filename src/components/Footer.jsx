9de1f4b1ae608345be98730f6a6b9d665db4dcb4
sekajikmaynard@gmail.com <59362984+sekaji@users.noreply.github.com>
2021-10-13 20:28:02 -0400

    Up until footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,79 @@
+import { Facebook, Instagram, Pinterest, Twitter } from '@material-ui/icons';
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    display: flex;
+`;
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+`;
+
+const Right = styled.div`
+    flex: 1;
+`;
+
+const Center = styled.div`
+    flex: 1;
+`;
+
+const Logo = styled.h1`
+
+`;
+
+const Desc = styled.p`
+    margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+    display: flex;
+`;
+
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${props=>props.color};
+`;
+const Footer = () => {
+    return (
+        <Container>
+            <Left>
+                <Logo>
+                    KI.
+                </Logo>
+                <Desc> There are many variations of passages of Lorem Ipsum available, but
+                    the majority have suffered alteration in some form, by injected
+                    humour, or randomised words which don’t look even slightly believable.
+                </Desc>
+                <SocialContainer>
+                    <SocialIcon color="3b5999">
+                        <Facebook />
+                    </SocialIcon>
+                    <SocialIcon color="e4405f">
+                        <Instagram />
+                    </SocialIcon>
+                    <SocialIcon color="55acee">
+                        <Twitter />
+                    </SocialIcon>
+                    <SocialIcon color="e60023">
+                        <Pinterest />
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center>
+
+            </Center>
+            <Right>
+
+            </Right>
+        </Container>
+    )
+}
+
+export default Footer
